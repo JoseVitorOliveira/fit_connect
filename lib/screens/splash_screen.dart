c1b8f4ce9ea0764444bc6f6287c600341a042e83
José Vítor <jose.vitor.oliveira.44@gmail.com>
2024-04-07 23:41:31 -0300

    splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,66 @@
+import 'package:fit_connect/screens/sign_in.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (_) => const SignIn(),
+      ));
+    });
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: Color(0xff00B1C3),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              "assets/images/logo.svg",
+              height: 90,
+              width: 90,
+            ),
+            const Text(
+              "Fit Connect",
+              style: TextStyle(
+                  fontSize: 48,
+                  height: 0.001,
+                  color: Colors.white,
+                  fontFamily: "Kanit",
+                  fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(height: 30),
+            Image.asset("assets/images/img1.png")
+          ],
+        ),
+      ),
+    );
   }
 }
