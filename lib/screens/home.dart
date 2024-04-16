5c811a8d581ad67c1b3c2a19d77a6390df8e955d
José Vítor <jose.vitor.oliveira.44@gmail.com>
2024-04-15 22:06:06 -0300

    VeticalList - home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,7 +12,7 @@ class Home extends StatelessWidget {
           const Padding(
             padding: EdgeInsets.all(16.0),
             child: Text(
-              "Profiles",
+              "Profissionais",
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
@@ -50,7 +50,82 @@ class Home extends StatelessWidget {
               }).toList(),
             ),
           ),
+          const SizedBox(height: 16),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              "Perto de você",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const Expanded(
+            child: VerticalList(),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class VerticalList extends StatelessWidget {
+  const VerticalList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: List.generate(20, (index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: double.infinity,
+              height: 100,
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 198, 236, 255),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    width: 80,
+                    height: 80,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey[300],
+                      border: Border.all(
+                        color: Colors.grey[400]!,
+                        width: 2.0,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Person ${index + 1}",
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      Text(
+                        "Academia ${index + 1}",
+                        style:
+                            const TextStyle(fontSize: 14, color: Colors.indigo),
+                      ),
+                      const Text(
+                        "Horário Disponível: XX:XX - YY:YY",
+                        style: TextStyle(fontSize: 14, color: Colors.indigo),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        }),
       ),
     );
   }
